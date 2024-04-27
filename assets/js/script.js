ff8f9e2900869297fd27f86ee4b6555a5bd68a86
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-04-28 00:03:18 +0100

    feat: basic draw card and add to pile features added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+let shuffledPile = [];
 let playerHand = [];
-let layPile = [];
+let discardPile = [];
+let deckSize = 52;
+
+let cardChoice;
+let topCard;
+
+let randomizer;
+let newShuffledDeckKey;
+
 
 /**
  * The following function creates a new shuffled deck from the Deck of Cards API
@@ -11,61 +20,137 @@ $(document).ready(function () {
 
     shuffleDeck(); // shuffles deck once document is ready   
 
-    $(".card-image").on("click", function () {
+    $(document).on("click", ".card-image", function() {
+        cardChoiceBuffer($(this).attr("id"));
         $(this).addClass("card-choice");
         $("#decision-text").show();
         $(".button-container").show();
-        $(".card-image").off();
+         $(document).off("click", ".card-image");
     });
-
+    
     $("#no").on("click", function () {
+        cardChoice = undefined;
         $(".card-image").removeClass("card-choice");
         $("#decision-text").hide();
         $(".button-container").hide();
-        $(".card-image").on("click", function () {
+        $(document).on("click", ".card-image", function() {
+            cardChoiceBuffer($(this).attr("id"));
             $(this).addClass("card-choice");
             $("#decision-text").show();
             $(".button-container").show();
-            $(".card-image").off();
+            $(document).off("click", ".card-image");
         });
     });
     $("#yes").on("click", function() {
-        $(".card-choice").html(" ");
-        $(".card-image").removeClass("card-choice");
+        addToPile();
+        $(".card-choice").remove();
         $("#decision-text").hide();
         $(".button-container").hide();
-        $(".card-image").on("click", function () {
+        $(document).on("click", ".card-image", function() {
+            cardChoiceBuffer($(this).attr("id"));
             $(this).addClass("card-choice");
             $("#decision-text").show();
             $(".button-container").show();
-            $(".card-image").off();
+            $(document).off("click", ".card-image");
         });
         
+
     })
 });
 
 
-const shuffleDeck = () => {
+const shuffleDeck = async () => {
     fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(response => response.json())
         .then(response => {
             newShuffledDeckKey = response.deck_id;
-            console.log(response);
-            dealHand();
+            fetch(`https://www.deckofcardsapi.com/api/deck/${newShuffledDeckKey}/draw/?count=52`)
+            .then(response => response.json())
+            .then(deck => {
+                for (let i = 0; i < 52; i++) {
+                    shuffledPile.push({"value": deck.cards[i].value, "suit": deck.cards[i].suit, "image": deck.cards[i].image})
+                };
+            console.log(shuffledPile);
+            dealInitialHand();
         });
+    });
 };
 
-const dealHand = () => {
-    fetch(`https://www.deckofcardsapi.com/api/deck/${newShuffledDeckKey}/draw/?count=8`)
-        .then(response => response.json())
-        .then(card => {
-            for (let i = 0; i < 8; i++) {
-                $(`#card-image-${i}`).html(`
-            <img src="${card.cards[i].image}" alt="${card.cards[i].value} of ${card.cards[i].suit}" width="113" height="157">`);
-            playerHand.push({"value": card.cards[i].value, "suit": card.cards[i].suit})
-            };
-            console.log(card);
-            console.log(playerHand);
-        });
+
+const dealInitialHand = () => {
+    for (let i = 0; i < 8; i++) {
+        randomizer = Math.floor(Math.random() * deckSize);
+        playerHand.push(shuffledPile[randomizer]);
+        shuffledPile.splice(randomizer, 1)
+        console.log(randomizer);
+        deckSize -=1;
+    };
+    console.log(playerHand);
+    console.log(shuffledPile);
+    console.log(deckSize);
+    randomizer = undefined;
+    displayInitialHand(playerHand);
 };
 
+const displayInitialHand = (hand) => {
+    for (let card of hand) {
+        $(".player-hand").append(`
+        <div class="col-1 card-image" id="${card.value} of ${card.suit}">
+        <img src="${card.image}" width="113" height="157">
+        </div>
+        `);
+    }
+}
+
+
+
+
+const drawCard = () => {
+    randomizer = Math.floor(Math.random() * deckSize);
+    playerHand.push(shuffledPile[randomizer]);
+    shuffledPile.splice(randomizer, 1)
+    deckSize -=1;
+    console.log(playerHand);
+    console.log(shuffledPile);
+    console.log(deckSize);
+    $(".player-hand").append(`
+        <div class="col-1 card-image" id="${playerHand[playerHand.length-1].value} of ${playerHand[playerHand.length-1].suit}">
+        <img src="${playerHand[playerHand.length-1].image}" width="113" height="157">
+        </div>
+        `);
+    randomizer = undefined;
+    
+};
+
+
+const cardChoiceBuffer = (string) => {
+let words = [];
+for (let word of string.split(" ")) {
+    words.push(word);
+};
+let buffer = playerHand.filter(card => {
+    return (card.value === words[0] && card.suit === words[2])
+});
+cardChoice = buffer[0];
+};
+
+
+const addToPile = () => {
+    if (topCard == undefined) {
+        topCard = cardChoice;
+        console.log(topCard);
+    } else {
+        discardPile.push(topCard);
+        topCard = cardChoice;
+        console.log(topCard);
+        console.log(discardPile);
+    }
+    let index = playerHand.indexOf(topCard);
+    playerHand.splice(index, 1);
+    
+    $(".card-image-pile").html(`
+    <img src="${topCard.image}" width="113" height="157">
+    `)
+    cardChoice = undefined;
+    console.log(playerHand);
+}
