7a877b377b4b666d4d09ae83afd0bc346894594c
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-04-28 19:30:54 +0100

    feat: added more game functionality (deck looping and card card layering conditions)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let shuffledPile = [];
 let playerHand = [];
 let discardPile = [];
+
+let discardDeckSize;
 let deckSize = 52;
 
 let cardChoice;
@@ -11,7 +13,7 @@ let newShuffledDeckKey;
 
 
 /**
- * The following function creates a new shuffled deck from the Deck of Cards API
+ * jQuery code that calls the onClick functions via Event Listners
  */
 $(document).ready(function () {
 
@@ -20,44 +22,50 @@ $(document).ready(function () {
 
     shuffleDeck(); // shuffles deck once document is ready   
 
-    $(document).on("click", ".card-image", function() {
+    $(document).on("click", ".clickable", function () {
         cardChoiceBuffer($(this).attr("id"));
         $(this).addClass("card-choice");
         $("#decision-text").show();
         $(".button-container").show();
-         $(document).off("click", ".card-image");
+        $(document).off("click", ".clickable");
     });
-    
+
     $("#no").on("click", function () {
         cardChoice = undefined;
         $(".card-image").removeClass("card-choice");
         $("#decision-text").hide();
         $(".button-container").hide();
-        $(document).on("click", ".card-image", function() {
+        $(document).on("click", ".clickable", function () {
             cardChoiceBuffer($(this).attr("id"));
             $(this).addClass("card-choice");
             $("#decision-text").show();
             $(".button-container").show();
-            $(document).off("click", ".card-image");
+            $(document).off("click", ".clickable");
         });
     });
-    $("#yes").on("click", function() {
+    $("#yes").on("click", function () {
         addToPile();
         $(".card-choice").remove();
         $("#decision-text").hide();
         $(".button-container").hide();
-        $(document).on("click", ".card-image", function() {
+        $(document).on("click", ".clickable", function () {
             cardChoiceBuffer($(this).attr("id"));
             $(this).addClass("card-choice");
             $("#decision-text").show();
             $(".button-container").show();
-            $(document).off("click", ".card-image");
+            $(document).off("click", ".clickable");
         });
-        
+
 
     })
 });
 
+
+/**
+ * The following functions fetch the deck data from the Deck of Cards API, then "draws" all of these cards into a ShuffledPile array. The players hand is then drawn by randomly selecting
+ * an index of that array, pushing that item to the playerHand array, then deleting that object from the ShuffledPile array. The image of the card is then displayed to the DOM by obtaining the image
+ * data inside the card object, and manipulating the HTML to include an <img> with this data as it's source (src).
+ */
 
 const shuffleDeck = async () => {
     fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -65,92 +73,136 @@ const shuffleDeck = async () => {
         .then(response => {
             newShuffledDeckKey = response.deck_id;
             fetch(`https://www.deckofcardsapi.com/api/deck/${newShuffledDeckKey}/draw/?count=52`)
-            .then(response => response.json())
-            .then(deck => {
-                for (let i = 0; i < 52; i++) {
-                    shuffledPile.push({"value": deck.cards[i].value, "suit": deck.cards[i].suit, "image": deck.cards[i].image})
-                };
-            console.log(shuffledPile);
-            dealInitialHand();
+                .then(response => response.json())
+                .then(deck => {
+                    for (let i = 0; i < 52; i++) {
+                        shuffledPile.push({ "value": deck.cards[i].value, "suit": deck.cards[i].suit, "image": deck.cards[i].image })
+                    };
+                    dealInitialHand();
+                });
         });
-    });
 };
-
 
 const dealInitialHand = () => {
     for (let i = 0; i < 8; i++) {
         randomizer = Math.floor(Math.random() * deckSize);
         playerHand.push(shuffledPile[randomizer]);
         shuffledPile.splice(randomizer, 1)
-        console.log(randomizer);
-        deckSize -=1;
+        deckSize -= 1;
     };
-    console.log(playerHand);
-    console.log(shuffledPile);
-    console.log(deckSize);
+    randomizer = Math.floor(Math.random() * deckSize);
+    topCard = shuffledPile[randomizer];
+    $(".card-image-pile").html(`
+    <img src="${topCard.image}" width="113" height="157">
+    `);
     randomizer = undefined;
-    displayInitialHand(playerHand);
-};
+    console.log("Current Pile")
+    console.log(shuffledPile);
+    console.log("Current Player Hand");
+    console.log(playerHand);
+    console.log("Current Top Card");
+    console.log(topCard);
 
-const displayInitialHand = (hand) => {
+    displayHand(playerHand);
+};
+const displayHand = (hand) => {
     for (let card of hand) {
-        $(".player-hand").append(`
-        <div class="col-1 card-image" id="${card.value} of ${card.suit}">
+        if (card.value == "8" || ((card.suit === topCard.suit) || (card.value === topCard.value))) {
+            $(".player-hand").append(`
+        <div class="col-1 card-image clickable" id="${card.value}-of-${card.suit}">
         <img src="${card.image}" width="113" height="157">
         </div>
         `);
-    }
-}
+        } else {
+            $(".player-hand").append(`
+        <div class="col-1 card-image not-clickable" id="${card.value}-of-${card.suit}">
+        <img src="${card.image}" width="113" height="157">
+        </div>
+        `);
+        };
 
+    };
+};
 
 
 
 const drawCard = () => {
+    if (shuffledPile.length < 1) {
+        totalLoopReps = discardPile.length
+        discardDeckSize = discardPile.length;
+        for (let i = 0; i < totalLoopReps; i++) {
+            randomizer = Math.floor(Math.random() * discardDeckSize);
+            shuffledPile.push(discardPile[randomizer]);
+            discardPile.splice(randomizer, 1);
+            discardDeckSize -= 1;
+        };
+        randomizer = undefined;
+        randomFunction();
+    } else {
+        randomFunction();
+    };
+    console.log("Current Deck size");
+    console.log(shuffledPile);
+    console.log("Current Player Hand");
+    console.log(playerHand);
+};
+
+const randomFunction = () => {
+    deckSize = shuffledPile.length;
     randomizer = Math.floor(Math.random() * deckSize);
     playerHand.push(shuffledPile[randomizer]);
-    shuffledPile.splice(randomizer, 1)
-    deckSize -=1;
-    console.log(playerHand);
-    console.log(shuffledPile);
-    console.log(deckSize);
-    $(".player-hand").append(`
-        <div class="col-1 card-image" id="${playerHand[playerHand.length-1].value} of ${playerHand[playerHand.length-1].suit}">
-        <img src="${playerHand[playerHand.length-1].image}" width="113" height="157">
+    shuffledPile.splice(randomizer, 1);
+    deckSize -= 1;
+    if (playerHand[playerHand.length - 1].value == "8" || ((playerHand[playerHand.length - 1].value === topCard.value) || (playerHand[playerHand.length - 1].suit === topCard.suit))) {
+        $(".player-hand").append(`
+        <div class="col-1 card-image clickable" id="${playerHand[playerHand.length - 1].value}-of-${playerHand[playerHand.length - 1].suit}">
+        <img src="${playerHand[playerHand.length - 1].image}" width="113" height="157">
         </div>
         `);
+    } else {
+        $(".player-hand").append(`
+        <div class="col-1 card-image not-clickable" id="${playerHand[playerHand.length - 1].value}-of-${playerHand[playerHand.length - 1].suit}">
+        <img src="${playerHand[playerHand.length - 1].image}" width="113" height="157">
+        </div>
+        `);
+    };
+
     randomizer = undefined;
-    
 };
-
-
 const cardChoiceBuffer = (string) => {
-let words = [];
-for (let word of string.split(" ")) {
-    words.push(word);
+    let words = [];
+    for (let word of string.split("-")) {
+        words.push(word);
+    };
+    let buffer = playerHand.filter(card => {
+        return (card.value === words[0] && card.suit === words[2])
+    });
+    cardChoice = buffer[0];
 };
-let buffer = playerHand.filter(card => {
-    return (card.value === words[0] && card.suit === words[2])
-});
-cardChoice = buffer[0];
-};
-
 
 const addToPile = () => {
     if (topCard == undefined) {
         topCard = cardChoice;
+        console.log("Current Top Card");
         console.log(topCard);
     } else {
         discardPile.push(topCard);
         topCard = cardChoice;
+        // Console testers
+        console.log("Current Top Card");
         console.log(topCard);
+        console.log("Current Discard Pile")
         console.log(discardPile);
-    }
+    };
     let index = playerHand.indexOf(topCard);
     playerHand.splice(index, 1);
-    
+
     $(".card-image-pile").html(`
     <img src="${topCard.image}" width="113" height="157">
-    `)
+    `);
     cardChoice = undefined;
+    $(".player-hand").html("");
+    displayHand(playerHand);
+    console.log("Current player hand");
     console.log(playerHand);
-}
+};
